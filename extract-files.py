3720bfe3c0d878a88e55b19495089f8d30c56b6e
Andrey Sprynga <rtx4d@aospa.co>
2025-05-19 16:25:16 +0700

    nx606j: Init Check Elf
    Change-Id: Ic15597ee62a1eea4197bf2a250f74ad17a8e644f

--- extract-files.py
+++ extract-files.py
@@ -8,18 +8,27 @@ from extract_utils.fixups_blob import (
     blob_fixup,
     blob_fixups_user_type,
 )
+from extract_utils.fixups_lib import (
+    lib_fixup_remove,
+    lib_fixups,
+    lib_fixups_user_type,
+)
 from extract_utils.main import (
     ExtractUtils,
     ExtractUtilsModule,
 )
 
 namespace_imports = [
+    'hardware/qcom-caf/common/libqti-perfd-client',
+    'hardware/qcom-caf/sdm845',
+    'hardware/nubia',
+    'vendor/qcom/opensource/display',
     'vendor/nubia/sdm845-common'
 ]
 
 blob_fixups: blob_fixups_user_type = {
     'vendor/lib/hw/audio.primary.sdm845.so': blob_fixup()
-        .replace_needed('libcutils.so', 'libprocessgroup.so'),
+        .add_needed('libprocessgroup.so'),
     'vendor/lib64/libgoodixfingerprintd_binder.so': blob_fixup()
         .add_needed('libbinder_shim.so'),
     'vendor/lib64/libSNPE.so': blob_fixup()
@@ -28,14 +37,20 @@ blob_fixups: blob_fixups_user_type = {
         .remove_needed('libmedia.so++.so'),
     'vendor/lib64/vendor.goodix.hardware.fingerprintextension@1.0.so': blob_fixup()
         .replace_needed('libhidlbase.so', 'libhidlbase-v32.so'),
+    'vendor/lib/camera/components/com.nubia.node.realtimeaicamera.so': blob_fixup()
+        .add_needed('libcutils.so'),
+    'vendor/lib/hw/camera.qcom.so': blob_fixup()
+        .clear_symbol_version('remote_handle64_close')
+        .clear_symbol_version('remote_handle64_invoke')
+        .clear_symbol_version('remote_handle64_open')
 }  # fmt: skip
 
 module = ExtractUtilsModule(
     'nx606j',
     'nubia',
-    namespace_imports=namespace_imports,
     blob_fixups=blob_fixups,
-    check_elf=False,
+    lib_fixups=lib_fixups,
+    namespace_imports=namespace_imports,
 )
 
 if __name__ == '__main__':
